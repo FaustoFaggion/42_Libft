d4d12297a86f13fd2d388e21067a9c129647c3fe
Fausto Faggion <faustofaggion@hotmail.com>
2021-07-29 20:10:06 -0300

    add ft_atoi

--- README.md
+++
@@ -1,2 +0,0 @@
-# 42_Libft
-Your very first own library
---
+++ ft_atoi.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+#include <stdio.h>
+
+static  int ft_isspace(char c)
+{
+    if(c == '\t' || c == '\v' || c == '\r' 
+		|| c == '\n' || c == '\f' || c == ' ')
+        return(1);
+    return(0);
+}
+
+int ft_atoi(const char *ptr)
+{
+    int res;
+    int sign;
+
+    res = 0;
+    sign = 1;
+    while(ft_isspace(*ptr))
+    {
+        ptr++;
+    }
+    if(*ptr == '+' || *ptr == '-') //Is there while in this function???
+    {
+        if(*ptr == '-')
+            sign = -1;
+        ptr++;
+    }
+    while(ft_isdigit(*ptr))
+    {
+        res = (res * 10) + (*ptr - 48);
+        ptr++;
+    }
+    return(sign * res);
+}
