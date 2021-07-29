dfe5a8aef00eb12de56391a20493760a647a075a
Fausto Faggion <faustofaggion@hotmail.com>
2021-07-29 20:26:54 -0300

    add ft_isalpha

---
+++ ft_isalpha.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+static int	ft_isupper(int c)
+{
+	return(c >= 'A' && c <= 'Z');
+}
+
+static int	ft_islower(int c)
+{
+	return(c >= 'a' && c <= 'z');
+}
+
+int ft_isalpha(int c)
+{
+	return(ft_isupper(c) || ft_islower(c));
+}
