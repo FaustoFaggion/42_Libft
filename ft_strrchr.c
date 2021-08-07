0b6bea9f003f0f4ca7992f46b807348be9aec728
Fausto Faggion <faustofaggion@hotmail.com>
2021-08-07 17:17:47 -0300

    update tripolli test 1 ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -19,6 +19,8 @@ char	*ft_strrchr(const char *s, int c)
 {
 	size_t	len;
 	
+	if(!s || !c)
+		return(NULL);
 	len = ft_strlen(s);
 	while (len > 0)
 	{
@@ -26,5 +28,7 @@ char	*ft_strrchr(const char *s, int c)
 			return ((char *)s + len);
 		len--;
 	}
+	if (len == 0 && *((char *)s + len) != (char)c )
+		return(NULL);
 	return ((char *)s + len);
 }
