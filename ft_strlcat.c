2f2e9c2d6cc5359e27d4ace9e6cdf5bcde013494
Fausto Faggion <faustofaggion@hotmail.com>
2021-08-07 20:10:21 -0300

    update tripolli tests ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -9,8 +9,9 @@ size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 
 	len = ft_strlen(dest);
 	if (dstsize <= len)
-		return (len + ft_strlen(src));
+		return (dstsize + ft_strlen(src));
 	i = 0;
+	if (dstsize > len)
 	while (dstsize > 1 && src[i] != '\0')
 	{
 		dest[len + i] = src[i];
@@ -20,15 +21,13 @@ size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 	dest[len + i] = '\0';
 	return (len + ft_strlen(src));
 }
-/*
+
 int main(void)
 {
 	char dest[30]; memset(dest, 0, 30);
 	char * src = (char *)"AAAAAAAAA";
-	dest[0] = 'B';
-	memset(dest, 'C', 15);
+	memset(dest, 'B', 4);
 
-	printf("%ld\n\n", (size_t )ft_strlcat(dest, src, 17));
+	printf("%ld", (size_t )ft_strlcat(dest, src, 1));
 	printf(" -> %s\n", dest);
 }
-*/
