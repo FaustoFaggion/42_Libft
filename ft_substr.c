1ed535047478fd9eff5e8b4e7915a0196141a5c8
Fausto Faggion <faustofaggion@hotmail.com>
2021-08-07 21:21:41 -0300

    update tripolli tests ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -1,4 +1,5 @@
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -7,12 +8,29 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (s == NULL)
 		return (NULL);
-	size = len + 1;
+	if(start > ft_strlen(s))
+		return (ft_strdup(""));
+	if((ft_strlen(s) - start) < len)
+		size = (ft_strlen(s) - start) + 1;
+	else
+		size = len + 1;
 	substr = (char *)malloc(size * sizeof(char));
 	if (!substr)
 	{
 		return (NULL);
 	}
+
 	ft_strlcpy(substr, &s[start], size);
 	return (substr);
 }
+/*
+int main(void)
+{
+	char * str = ft_strdup("1");
+	char *s = ft_substr(str, 42, 42000000);
+
+	printf("%s", s);
+	free(s);
+	return(0);
+}
+*/
