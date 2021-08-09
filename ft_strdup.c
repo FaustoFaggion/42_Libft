93e55673ea40f41106192a56dda5e93f81973004
Fausto Faggion <faustofaggion@hotmail.com>
2021-08-09 15:28:45 -0300

    update itoa.c for tripoille

--- ft_strdup.c
+++ ft_strdup.c
@@ -5,16 +5,11 @@ char	*ft_strdup(const char *s)
 {
 	size_t	len;
 	char	*mem;
-	size_t	i;
 
 	len = ft_strlen(s);
 	mem = (char *)malloc(sizeof(char) * (len + 1));
-	i = 0;
-	while (s[i] != '\0')
-	{
-		mem[i] = s[i];
-		i++;
-	}
-	mem[i] = '\0';
-	return ((char *)mem);
+	if(!mem)
+		return(NULL);
+	ft_strlcpy(mem, s, len + 1);
+	return (mem);
 }
