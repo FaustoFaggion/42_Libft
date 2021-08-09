9ffe0a110a99a6a88f9968be59d1212475688319
Fausto Faggion <faustofaggion@hotmail.com>
2021-08-09 16:40:00 -0300

    update itoa.c for tripoille

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -4,12 +4,17 @@ void	ft_putnbr_fd(int n, int fd)
 {
 
 	if(n == INT_MIN)
+	{	
 		ft_putstr_fd("-2147483648", fd);
+		return;
+	}
 	if (n < 0)
+	{
 		ft_putchr_fd('-', fd);
 		n = -n;
+	}
 	if (n < 10)
-		ft_putchr(n + '0', fd);	
+		ft_putchr_fd(n + '0', fd);	
 		return;
 	if(n >= 10 && n <= INT_MAX)
 		ft_putnbr_fd(n / 10, fd);
