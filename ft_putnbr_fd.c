8dcaef27f4234e1e1ee44592acf0fbb0d23b1ec2
Fausto Faggion <faustofaggion@hotmail.com>
2021-08-09 16:32:49 -0300

    update itoa.c for tripoille

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,14 +1,17 @@
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)101 10 1 
 {
-	char	*nb;
-	int		len;
 
 	if(n == INT_MIN)
 		ft_putstr_fd("-2147483648", fd);
-	if(n > INT_MIN && n <= INT_MAX)
-		nb = ft_itoa(n);
-	len = ft_strlen(nb);
-	write (fd, nb, len);
+	if (n < 0)
+		ft_putchr('-', fd);
+		n = -n;
+	if (n < 10)
+		ft_putchr(n + '0', fd);	
+		break;
+	if(n >= 10 && n <= INT_MAX)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putnbr_fd(n % 10, fd)
 }
