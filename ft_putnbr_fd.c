08e452354a75c52481b995a52843bf1521442910
Fausto Faggion <faustofaggion@hotmail.com>
2021-08-09 16:07:22 -0300

    update itoa.c for tripoille

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -5,7 +5,10 @@ void	ft_putnbr_fd(int n, int fd)
 	char	*nb;
 	int		len;
 
-	nb = ft_itoa(n);
+	if(n == INT_MIN)
+		return(ft_putstr("-2147483648"))
+	if(n > INT_MIN && n <= INT_MAX)
+		nb = ft_itoa(n);
 	len = ft_strlen(nb);
 	write (fd, nb, len);
 }
