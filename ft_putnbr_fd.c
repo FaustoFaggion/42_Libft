62f8d9d16ae9767b217099168de0b044b759b8a4
Fausto Faggion <faustofaggion@hotmail.com>
2021-08-10 11:42:49 -0300

    update ft_putnbr_fd.c for tripoille

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -11,15 +11,13 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	ft_putnbr_fd(int n, int fd)
 {
-
-	if(n == INT_MIN)
+	if (n == INT_MIN)
 	{	
 		ft_putstr_fd("-2147483648", fd);
-		return;
+		return ;
 	}
 	if (n < 0)
 	{
@@ -28,23 +26,10 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	if (n < 10)
 	{
-		ft_putchar_fd(n + '0', fd);	
-		return;
+		ft_putchar_fd(n + '0', fd);
+		return ;
 	}
-	if(n >= 10 && n <= INT_MAX)
+	if (n >= 10 && n <= INT_MAX)
 		ft_putnbr_fd(n / 10, fd);
 	ft_putnbr_fd(n % 10, fd);
 }
-
-/*
-int main(void)
-{
-	int n;
-	
-	n = 80;
-
-	ft_putnbr_fd(n, 1);
-	return(0);
-
-}
-*/
