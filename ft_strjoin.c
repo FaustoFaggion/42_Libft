0c8a6182cbc6ca1fcfb7d52841a7b78fadf03b1b
Fausto Faggion <faustofaggion@hotmail.com>
2021-08-10 17:30:36 -0300

    review  Libftest

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -23,7 +23,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	len_s2 = ft_strlen(s2);
 	tot_size = (len_s1 + len_s2 + 1);
 	new = (char *)malloc(tot_size * sizeof(char));
-	if(!new)
+	if (!new)
 		return (NULL);
 	ft_strlcpy(new, s1, (len_s1 + 1));
 	ft_strlcpy(&new[len_s1], s2, (len_s2 + 1));
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -34,13 +34,3 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return (0);
 }
-
-int main(void)
-{
-	char	s1[] = "1234";
-	char	s2[] = "1234";
-	size_t	n = 9;
-
-	printf("%d\n", ft_strncmp(s1, s2, n));
-	return (0);	
-}
