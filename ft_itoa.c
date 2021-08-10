c77a757354412711a60a1661213b4bcebc949722
Fausto Faggion <faustofaggion@hotmail.com>
2021-08-10 15:09:57 -0300

    review ft_itoa.c war-machine

--- ft_itoa.c
+++ ft_itoa.c
@@ -13,7 +13,26 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-//#include <stdio.h>
+
+static size_t	ft_numlen(int num);
+static char		*main_itoa(char *str, size_t n_digits, int n);
+
+char	*ft_itoa(int n)
+{
+	size_t	n_digits;
+	char	*str;
+
+	if (n == INT_MIN)
+		return (ft_strdup("-2147483648"));
+	if (n == 0)
+		return (ft_strdup("0"));
+	n_digits = ft_numlen(n);
+	str = (char *)malloc((n_digits + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	main_itoa(str, n_digits, n);
+	return (str);
+}
 
 static size_t	ft_numlen(int num)
 {
@@ -33,19 +52,8 @@ static size_t	ft_numlen(int num)
 	return (len);
 }
 
-char	*ft_itoa(int n)
+static char	*main_itoa(char *str, size_t n_digits, int n)
 {
-	size_t	n_digits;
-	char	*str;
-
-	if (n == INT_MIN)
-		return (ft_strdup("-2147483648"));
-	if (n == 0)
-		return (ft_strdup("0"));
-	n_digits = ft_numlen(n);
-	str = (char *)malloc((n_digits + 1) * sizeof(char));
-	if (!str)
-		return (NULL);
 	if (n < 0)
 	{
 		str[0] = '-';
@@ -63,12 +71,3 @@ char	*ft_itoa(int n)
 		str[n_digits] = n + '0';
 	return (str);
 }
-/*
-int main(void)
-{
-	char *res = ft_itoa(9);
-		
-	printf("%s\n",res);
-	return(0);
-}
-*/
