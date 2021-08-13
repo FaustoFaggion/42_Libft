9baeafd511b088c85982accd1bbdf7130de7c313
Fausto Faggion <faustofaggion@hotmail.com>
2021-08-13 13:17:36 -0300

    add after conference

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: fagiusep <faustofaggion@hotmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 19:47:57 by fagiusep          #+#    #+#             */
-/*   Updated: 2021/08/13 13:15:03 by fagiusep         ###   ########.fr       */
+/*   Updated: 2021/08/13 13:17:29 by fagiusep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 	{
 		return ((char *)&haystack[0]);
 	}
-	if(n == 0)
-		return (NULL);
 	i = 0;
 	while (haystack[i] != '\0' && i <= (n - ft_strlen(needle)))
 	{
@@ -37,5 +35,5 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 		}
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
